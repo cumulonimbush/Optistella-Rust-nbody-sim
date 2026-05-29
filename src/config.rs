e0e98cf8291c7001a0a5c9b4827f3c85028a38b9
mehmedaltug <mehmed.ede@hotmail.com>
2026-05-29 19:14:12 +0300

    re-added easy control constants

---
+++ src/config.rs
@@ -0,0 +1,5 @@
+pub static BODY_MESH_RADIUS:f32 = 0.4;
+pub static BODY_COUNT:i32 = 500;
+pub static BODY_VEL_RANGE:f32 = 15.0;
+pub static BODY_POS_RANGE:f32 = 60.0;
+pub static BODY_MASS_RANGE:[f32; 2] = [10.0, 1000.0];
