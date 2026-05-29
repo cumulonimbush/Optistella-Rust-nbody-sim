2598e56e894f44548bd4a1a68251f5a94c4f2dd0
Your Name <alikemal1128@hotmail.com>
2026-05-29 16:36:26 +0300

    feat: implement Barnes-Hut N-body simulation with octree spatial partitioning

--- src/config.rs
+++
@@ -1,6 +0,0 @@
-pub static STAR_COUNT: usize = 100000;
-pub static BARN_SIZE: usize = 100;
-pub static SPHERE_RADIUS: f32 = 0.3;
-pub static SUB_DIVISION: u32 = 5;
-pub static BARN_RADIUS: i32 = 1000;
-pub static GENERAL_RADIUS: i32 = 1000000;
