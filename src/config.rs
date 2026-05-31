afa3aa3c891b76fada580b6925e34553ea98bb5a
Your Name <alikemal1128@hotmail.com>
2026-05-31 22:02:23 +0300

    feat: simulation configuration and enable bevy dynamic linking

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
-pub static BODY_MESH_RADIUS:f32 = 0.4;
-pub static BODY_COUNT:i32 = 3000;
-pub static BODY_VEL_RANGE:f32 = 25.0;
-pub static BODY_POS_RANGE:f32 = 8000.0;
-pub static BODY_MASS_RANGE:[f32; 2] = [10.0, 1000.0];
-pub static SIMULATION_SPEED_FACTOR:f32 = 0.2;
+pub static BODY_MESH_RADIUS: f32 = 0.4;
+pub static BODY_COUNT: i32 = 10000;
+pub static BODY_VEL_RANGE: f32 = 25.0;
+pub static BODY_POS_RANGE: f32 = 1000.0;
+pub static BODY_MASS_RANGE: [f32; 2] = [10.0, 1000.0];
+pub static SIMULATION_SPEED_FACTOR: f32 = 0.2;
